2bf63ab25b1f956957778b69e29976524df51fba
Mike Irvine <michael.irvine.mai@gmail.com>
2019-01-22 17:04:28 -0800

    v1 complete with counterfactual analysis and basic intervention model

--- utils.py
+++ utils.py
@@ -3,90 +3,136 @@
     for pymc3.
 
 """
+import numpy as np
+
+#import some special methods
+from scipy.special import logit,expit
+import scipy.stats as stats
 
 
-from pymc3.model import modelcontext, get_named_nodes_and_relations
-from pymc3.util import is_transformed_name, get_untransformed_name
-from tqdm import tqdm
 
-
-def sample_prior(samples=500, model=None, vars=None, size=None,
-                 random_seed=None, progressbar=True):
-    """Generate samples from the prior of a model.
+def random_walk_prevalence(mu,sigma,initial_prevalence=0.01,shape=None):
+    '''Create a random walk prevalence process with drift mu and variance sigma
 
     Parameters
     ----------
-    samples : int
-        Number of samples from the prior to generate. Defaults to 500.
-    model : Model (optional if in `with` context)
-    vars : iterable
-        Variables for which to compute the posterior predictive samples.
-        Defaults to `model.named_vars`.
-    size : int
-        The number of random draws from the distribution specified by the
-        parameters in each sample of the trace.
-    random_seed : int
-        Seed for the random number generator.
-    progressbar : bool
-        Whether or not to display a progress bar in the command line.
+    mu : float
+        drift of random walk in invlogit space
+    sigma : float
+        standard deviation of random walk in invlogit space
+    shape : list
+        shape of random walk in format (n_samples,n_months)
 
     Returns
     -------
-    dict
-        Dictionary with the variables as keys. The values are arrays of prior samples.
-    """
+    numpy array
+        Samples of random walk process with shape (n_samples,n_months)
 
-    model = modelcontext(model)
+    '''
 
-    if vars is None:
-        vars = set(model.named_vars.keys())
+    # time-points and samples
+    n_samples,n_months = shape
 
-    if random_seed is not None:
-        np.random.seed(random_seed)
 
-    if progressbar:
-        indices = tqdm(range(samples))
+    # sample errors
+    es = np.random.normal(loc=mu,scale=sigma,size=(n_samples,n_months))
 
-    def step(model):
-        point = {}
-        for var in model.basic_RVs:
-            var_name = var.name
-            if hasattr(var, 'distribution'):
-                if is_transformed_name(var_name):
-                    val = var.distribution.dist.random(
-                        point=point, size=size)
-                    var_name_untranf = get_untransformed_name(var_name)
-                    point[var_name_untranf] = val
-                    val = var.distribution.transform_used.forward_val(val)
-                else:
-                    val = var.distribution.random(point=point, size=size)
-            else:
-                nn, _, _ = get_named_nodes_and_relations(var)
-                val = var.eval({model.named_vars[v]: point[v]
-                                for v in nn})
+    # create random walks with starting point equivalent to when prevalence is 0.01
+    ws = logit(initial_prevalence) + np.cumsum(es,axis=1)
 
-            point[var_name] = val
-        return point
+    # convert random walk into probability
+    ps = expit(ws)
 
-    try:
-        prior = {var: [] for var in vars}
-        for _ in indices:
-            point = step(model)
-            for var_name, var in model.named_vars.items():
-                if var_name in vars:
-                    if var_name in point:
-                        val = point[var_name]
-                    else:
-                        nn, _, _ = get_named_nodes_and_relations(var)
-                        val = var.eval({model.named_vars[v]: point[v]
-                                        for v in nn})
-                    prior[var_name].append(val)
+    return ps
 
-    except KeyboardInterrupt:
-        pass
+def generate_data(N_mean = 10000, N_sd = 1000, p0 = 0.01, mu_w=1.,
+                  sigma_w = 0.1, kappa_F = 0.25, kappa_N = 0.02, kappa_THN = 0.87,
+                 p_d = 0.1):
+    '''
+    Generate sample data for fentanyl adulterant model with Take-home naloxone kits
 
-    finally:
-        if progressbar:
-            indices.close()
+    Parameters
+    ----------
+    N_mean : float
+        mean of population
+    N_sd : float
+        sd of population
+    p0 : float
+        initial probability of fentanyl in supply
+    mu_w : float
+        drift of fentanyl (logit space)
+    sigma_w : float
+        scale of fentanyl (logit space)
+    kappa_F : float
+        probability of overdose on fentanyl
+    kappa_N : float
+        probability of overdose not on fentanyl
+    kappa_THN : float
+        probability of fentanyl use
+    p_d : float
+        probability of death following an overdose without intervention
 
-    return {k: np.asarray(v) for k, v in prior.items()}
+    Returns
+    -------
+
+    Dictionary of observables for a single sample
+
+
+    '''
+
+    # simulate fentanyl in illicit drug supply
+    pF = random_walk_prevalence(mu_w,sigma_w,initial_prevalence=p0,shape=(1,12))
+    # calculate overdose rate
+    od_rate = kappa_F*pF + kappa_N*(1-pF)
+
+    # simulate a population size
+    N = np.random.normal(loc=N_mean,scale=N_sd)
+
+    # simulate kits distributed
+    kits_distributed = np.cumsum(np.random.gamma(0.5,scale=300,size=(1,12)).round())
+
+    # simulate kits used
+    p_THN = kappa_THN*kits_distributed/N
+
+    # death rate modified due to use of THN kits
+    death_rate = (1. - p_THN)*p_d
+
+    # simulate overdoses
+    f_overdoses = np.random.binomial(N,kappa_F*pF)
+    nf_overdoses = np.random.binomial(N,kappa_N*(1.-pF))
+
+    overdoses = f_overdoses + nf_overdoses
+
+    # simulate deaths
+    f_deaths = np.random.binomial(f_overdoses,death_rate)
+    nf_deaths = np.random.binomial(nf_overdoses,death_rate)
+    deaths = f_deaths + nf_deaths
+
+    # kits used
+    kits_used = np.random.binomial(overdoses,p_THN)
+
+    return {'overdoses': overdoses.flatten(),'probability fentanyl': pF.flatten(),
+           'deaths': deaths.flatten(),'fentanyl deaths':f_deaths.flatten(),
+            'kits distributed': kits_distributed.flatten(),
+           'kits used':kits_used.flatten()}
+
+def save_generate_data(data,filename='./data/data_sample.csv'):
+    '''
+    Save data to CSV file from output of generate_data
+
+    Parameters
+    ----------
+    data : dict
+         output of method generate_data
+    filename : str
+        filename to save to
+    Returns
+    -------
+    None
+    '''
+    #add time
+    n_months=12
+    data['month'] = np.arange(1,n_months+1)
+
+    df = pd.DataFrame(data)
+    df[['month','overdoses','fentanyl deaths','deaths','kits distributed','kits used']].to_csv(filename,index=False)
